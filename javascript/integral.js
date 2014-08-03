ba7e09143ba7a1e1d3604ba70b21398392308e6f
alexisstdm <alexis.stdm@gmail.com>
2014-08-03 02:02:18 +0200

    Added interface for testing integral

--- javascript/integral.js
+++ javascript/integral.js
@@ -1,15 +1,17 @@
-function Integral(funcion, limite_inf, limite_sup, error=0.01){
-
+function integral(funcion, limite_inf, limite_sup){
+	alert("Entrando en Funcion");
+	var error = 0.001
 	var numero_iteraciones = Math.ceil(
 				Math.abs(limite_sup-limite_inf)/error*
 				Math.abs(funcion(limite_sup)-funcion(limite_inf)));
+	alert(numero_iteraciones)
 	var paso = Math.abs(limite_sup-limite_inf)/numero_iteraciones;
 	var x = limite_inf;
 	var suma = 0;
 	while (x<=limite_sup){
 		suma = suma + paso*(funcion(x)+funcion(x+paso))/2;
 		x = x + paso;
-	}
+	} 
 
 	return suma;
 }
