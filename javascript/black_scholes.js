bbdf81d457abcbea002df844b91522bda7123bac
alexisstdm <alexis.stdm@gmail.com>
2014-08-19 03:31:28 +0200

    We give greeks calculations for Black Scholes

--- javascript/black_scholes.js
+++ javascript/black_scholes.js
@@ -24,3 +24,39 @@ function priceBlackScholes(spot, strike, maturity, volatility, rate, dividend_ra
 	else
 		return 0.0;
 }
+
+function normal_density(x){
+	return 1.0/Math.sqrt(2.0 * Math.PI) * Math.exp(-1.0/2.0 * Math.pow(x, 2.0));
+}
+
+function greeksBlackScholes(spot, strike, maturity, volatility, rate, dividend_rate, type){
+
+	var resultado = {delta:0.0, vega:0.0, theta:0.0, rho:0.0, dividend_delta:0.0};
+
+	var d_plus = 1.0/volatility/Math.sqrt(maturity)*
+			Math.log(spot*Math.exp((rate-dividend_rate)*maturity)/strike)+
+			1.0/2.0*volatility*Math.sqrt(maturity);
+	var d_minus = 1.0/volatility/Math.sqrt(maturity)*
+			Math.log(spot*Math.exp((rate-dividend_rate)*maturity)/strike)-
+			1.0/2.0*volatility*Math.sqrt(maturity);
+
+	if (type == "call"){
+	   resultado["delta"] = Math.exp(-dividend_rate * maturity) * normal_dist(d_plus);
+	   resultado["vega"] = spot * Math.sqrt(maturity) * Math.exp(-dividend_rate * maturity) * normal_density(d_plus);
+	   resultado["theta"] = strike * Math.exp(-rate * maturity) * rate * normal_dist(d_minus) + 
+				spot * Math.exp(-dividend_rate * maturity) * (-dividend_rate * normal_dist(d_plus) +
+								volatility/(2.0 * Math.sqrt(maturity)) * normal_density(d_plus))
+	   resultado["rho"] = strike * maturity * Math.exp(-rate * maturity) * normal_dist(d_minus);
+	   resultado["dividend_delta"] = -spot * maturity * Math.exp(-dividend_rate * maturity) * normal_dist(d_plus);
+	}
+	else if (type == "put"){
+	   resultado["delta"] = Math.exp(-dividend_rate * maturity) * (normal_dist(d_plus) - 1);
+	   resultado["vega"] = spot * Math.sqrt(maturity) * Math.exp(-dividend_rate * maturity) * normal_density(d_plus);
+	   resultado["theta"] = -strike * Math.exp(-rate * maturity) * rate * normal_dist(-d_minus) + 
+				spot * Math.exp(-dividend_rate * maturity) * (dividend_rate * normal_dist(-d_plus) -
+								volatility/(2.0 * Math.sqrt(maturity)) * normal_density(-d_plus))
+	   resultado["rho"] = -strike * maturity * Math.exp(-rate * maturity) * normal_dist(-d_minus);
+	   resultado["dividend_delta"] = spot * maturity * Math.exp(-dividend_rate * maturity) * normal_dist(-d_plus);
+	}
+	return resultado;
+}
